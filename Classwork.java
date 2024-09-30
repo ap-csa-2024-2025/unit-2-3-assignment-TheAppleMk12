d9e96f9cb687fd0dffe1f9cfbe43e1e3b4d776d3
TheAppleMk12 <Abramhtran@gmail.com>
2024-09-30 11:19:06 -0700

    Completed first 3

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,39 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary" , 1));
+    System.out.println(firstLastN("apple" , 2));
+    System.out.println(delEnd("library", 5));
+    System.out.println(compareLower("Sterling", "Matierial"));
   }
 
+  public static String firstLastN(String word, int n)
+  {
+    String firstN = word.substring(0, n);
+    int indexOfNToLast = word.length() - n;
+    String lastN = word.substring(indexOfNToLast);
+
+    String output = firstN + lastN;
+    return output;
+  }
+
+  public static String delEnd(String word, int n)
+  {
+    int finalLength = word.length() - n;
+    String finalWord = word.substring(0, finalLength);
+
+    String output = finalWord;
+    return output;
+  }
+  public static String compareLower(String A, String B)
+  {
+    String lowerA = A.toLowerCase();
+    String lowerB = B.toLowerCase();
+    int difference = A.compareTo(B);
+
+    String output = A + "\n" + B + "\n" + "Result : " + difference;
+    return output;
+  }
   // write solutions to problems below
 
 }
